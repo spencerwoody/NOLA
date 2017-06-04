048d8edb90dd640258e3304b7b9da958e8e50595
Spencer Woody <spencer.woody@utexas.edu>
2017-06-04 16:55:26 -0500

    Distance matrix

--- nola.R
+++ nola.R
@@ -1,24 +1,33 @@
-# Created by Spencer Woody on 02 Jun 2017
+# Created by Spencer Woody on 04 Jun 2017
 
 library(ggplot2)
 library(ggmap)
+library(geosphere)
+library(RColorBrewer) # display.brewer.all
 
+# Read in data, change name of longitude column
 nola <- read.csv("nola.csv", header = T)
 names(nola)[2] <- "lon"
 
+### --------------------------------------------------------------------------
+### Map the stores
+### --------------------------------------------------------------------------
 
-NOLAmap_coords <- geocode("NOLA")
-
-NOLAmap_coords <- c(median(nola$lon), median(nola$lat))
-
+# Set bounds of map
 minlat <- min(nola$lat)
 minlon <- min(nola$lon)
 maxlat <- max(nola$lat)
 maxlon <- max(nola$lon)
 
-NOLAmap <- get_map(location = c(minlon, minlat, maxlon, maxlat), zoom = 13, col = "bw")
+# Create background for map image
+NOLAmap <- get_map(
+	location = c(minlon, minlat, maxlon, maxlat), 
+	zoom = 13, 
+	col = "bw"
+	)
 
-mymap <- ggmap(NOLAmap) + 
+# Plot locations of stores, color them by street name
+storemap <- ggmap(NOLAmap) + 
 geom_point(
 	data = nola, 
 	aes(x = lon, y = lat, col = factor(street)), 
@@ -30,8 +39,24 @@ scale_colour_brewer(
 	palette = "Set1"
 	) +
 theme(legend.position="bottom")
-mymap
+
+# Save this map to a PDF
+pdf("img/storemap.pdf")
+storemap
+dev.off()
+
+### --------------------------------------------------------------------------
+### Create distance matrices
+### --------------------------------------------------------------------------
+
+coords1 <- cbind(nola$lon[nola$street == 1], nola$lat[nola$street == 1])
+coords2 <- cbind(nola$lon[nola$street == 2], nola$lat[nola$street == 2])
+coords3 <- cbind(nola$lon[nola$street == 3], nola$lat[nola$street == 3])
+
+distmat1 <- distm(coords1) / 1609.34 # haversine distance, converted to miles
+distmat2 <- distm(coords2) / 1609.34
+distmat3 <- distm(coords3) / 1609.34
+
+distlist <- list(distmat1, distmat2, distmat3)
 
 
-
-s
